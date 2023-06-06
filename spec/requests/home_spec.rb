c24fdf556ab58dcca3bac4ac225b21996720a5a8
Donnell Reuben <donnell.reuben@icloud.com>
2023-06-06 12:02:16 -0500

    did some rspec stuff and properly installed boot as well as Hello World to root

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "is successful" do 
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
   end
 end
