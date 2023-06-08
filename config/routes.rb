d3469a6b46a598153b04e6e2e9d9d5e6a44c0ef7
Donnell Reuben <donnell.reuben@icloud.com>
2023-06-08 13:14:25 -0500

    added all pages and styled them accordingly as well as icon fill depending on which page you select

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get "dashboard" => "dashboard#index"
   get "search" => "search#index"
+  get "notifications" => "notifications#index"
+  get "messages" => "messages#index"
+  get "profile" => "profile#index"
+  get "tweets" => "tweets#index"
   
   resources :usernames, only: [:new, :update]
 end
