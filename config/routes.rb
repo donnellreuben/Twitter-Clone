17f008b261653aebb9dc236caca91ad38a184756
Donnell Reuben <donnell.reuben@icloud.com>
2023-06-08 12:45:13 -0500

    added search page and search bar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :tweets, only: :create
 
   get "dashboard" => "dashboard#index"
+  get "search" => "search#index"
   
   resources :usernames, only: [:new, :update]
 end
