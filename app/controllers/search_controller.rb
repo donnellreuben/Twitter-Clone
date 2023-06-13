8792eeb1605a2c7dcc22c62dd73c42a54632c318
Donnell Reuben <donnell.reuben@icloud.com>
2023-06-13 13:11:58 -0500

    started search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,4 +4,10 @@ class SearchController < ApplicationController
   def index
   end
 
+  def search
+    @query = params[:query]
+    @tweets = Tweet.where("content LIKE ?", "%#{@query}%")
+  end
+
+
 end
